8d315eb2f2637718c0b4634aff73abaadfb5da73
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-19 00:53:26 +0100

    My confession

--- FeedMeDaily/Controllers/HomeController.cs
+++ FeedMeDaily/Controllers/HomeController.cs
@@ -6,15 +6,16 @@ using System.Web.Mvc;
 
 namespace FeedMeDaily.Controllers
 {
+    //If this code works, then it was writen by Rosabel Olugbenga. If it doesn't, then i can only say one thing  "IT IS WHAT IT IS".
+
     [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            var Result = InsertIntoDb.Insert(1, "bread and egg", DateTime.Now);
-
-            return View();
+           return View();
         }
+
         public ActionResult Insert() 
         {
             var Result = InsertIntoDb.Insert(1, "bread and egg", DateTime.Now);
